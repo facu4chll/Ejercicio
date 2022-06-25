09e09c6680186745694c400b6f25a8fed5416399
Facu4chll <facundotomas69@gmail.com>
2022-06-25 16:13:38 -0300

    Update facu.js

--- facu.js
+++ facu.js
@@ -1,14 +1,12 @@
-var nombre = "Facundo";
-
-var edad = 20;
-
-desarrollador = false;
-
-const fecha_de_nacimiento = new Date(2002, 1, 12);
-
-var libro_favorito = {
-    titulo: "La lluvia sabe porqué",
-    autor: "María Fernanda Herdia",
-    fecha: 2014, 
-    url: 'https://www.sbs.com.ar/la-lluvia-sabe-por-que---zona-libre-9875457966/p'
-}
+const lista = [
+  "Facundo",
+  20,
+  false,
+  new Date(2002, 0, 12),
+  { 
+      titulo: "La lluvia sabe porqué",
+      autor: "María Fernanda Herdia",
+      fecha: new Date(2014, 0, 1),
+      url: 'https://www.sbs.com.ar/la-lluvia-sabe-por-que---zona-libre-9875457966/p'
+    },
+];
